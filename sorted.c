46806b8cc75c08885835f4164df41e1fc8b128bb
programmersgateway <vaidhyanathansm99@gmail.com>
2019-12-10 20:54:01 +0530

    added merge sorted array

---
+++ sorted.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+int c=0;
+void mergeArrays(int arr1[], int arr2[], int n1, 
+							int n2, int arr3[]) 
+{ 
+	int i = 0, j = 0, k = 0; 
+
+	while (i<n1 && j <n2) 
+	{ 
+		 
+		if (arr1[i] < arr2[j]) 
+			arr3[k++] = arr1[i++]; 
+		else
+            {
+                if(arr1[i]==arr2[j])
+                {
+                    arr3[k++] = arr2[j++]; 
+                    i++;
+                    c++;
+                }
+                else{
+                arr3[k++] = arr2[j++]; 
+                }
+		    }
+	} 
+
+	while (i < n1) 
+		arr3[k++] = arr1[i++]; 
+
+	while (j < n2) 
+		arr3[k++] = arr2[j++]; 
+} 
+
+int main() 
+{ 
+	int arr1[] = {1, 2, 3 , 4}; 
+	int n1 = sizeof(arr1) / sizeof(arr1[0]); 
+
+	int arr2[] = {3, 4, 5, 6,7,8}; 
+	int n2 = sizeof(arr2) / sizeof(arr2[0]); 
+
+	int arr3[n1+n2]; 
+	mergeArrays(arr1, arr2, n1, n2, arr3); 
+
+	printf("Array after merging\n"); 
+    
+        for (int i=0; i < (n1+n2)-c; i++) 
+		printf("%d ",arr3[i]); 
+	
+	return 0; 
+} 
